6dbcf8bf949e6162bbd6ec6b5c7e47b29b09ab95
GoncaloSilva-99 <goncalosilva23707@gmail.com>
2025-10-29 16:39:21 +0000

    Problemas Problemáticos no form de criar jogador

--- app/models/player_profile.rb
+++ app/models/player_profile.rb
@@ -2,4 +2,7 @@ class PlayerProfile < ApplicationRecord
   belongs_to :user
   belongs_to :club_profile, optional: true
   validates :name, presence: true
+
+  validates :role, presence: true, inclusion: {in: ["Futebol", "Andebol"]}
+  validates :dominant_foot_or_hand, inclusion: {in: ["Direito", "Esquerdo"]}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_26_194338) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_29_162309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -68,6 +68,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_26_194338) do
     t.bigint "club_profile_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sport"
+    t.string "secondary_position"
+    t.string "dominant_foot_or_hand"
     t.index ["club_profile_id"], name: "index_player_profiles_on_club_profile_id"
     t.index ["user_id"], name: "index_player_profiles_on_user_id"
   end
