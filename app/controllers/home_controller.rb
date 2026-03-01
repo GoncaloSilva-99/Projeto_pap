6662716fb98a7d942b2dde7582c774d1294671c5
GoncaloSilva-99 <goncalosilva23707@gmail.com>
2026-03-01 23:22:37 +0000

    Continuação posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,15 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @page = params[:page]&.to_i || 1
+      posts_per_page = 25
       
       if current_user.following.any?
         @posts = current_user.feed_posts(page: @page, per_page: 25)
       else
         @posts = Post.recent.limit(25).offset((@page - 1) * 25)
       end
+
+      @has_more_posts = @posts.length == posts_per_page
       
       @posts.each { |post| post.mark_as_viewed_by(current_user) } if @posts.present?
       @suggested_users = current_user.suggested_users_to_follow(limit: 6)
