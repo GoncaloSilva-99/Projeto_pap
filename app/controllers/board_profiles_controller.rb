beb869c9186272079f6c27028cc8de79c0b5e3c9
GoncaloSilva-99 <goncalosilva23707@gmail.com>
2026-03-17 14:52:02 +0000

    Board profiles terminado (com as mesmas restrições dos outros)

--- app/controllers/board_profiles_controller.rb
+++ app/controllers/board_profiles_controller.rb
@@ -8,6 +8,15 @@ class BoardProfilesController < ApplicationController
 
   # GET /board_profiles/1 or /board_profiles/1.json
   def show
+      @page = params[:page]&.to_i || 1
+      posts_per_page = 25
+      
+      @posts = Post.where(user_id: @board_profile.user.id).recent.offset((@page - 1) * posts_per_page)
+
+      @has_more_posts = @posts.length == posts_per_page
+
+      @post_comments = PostComment.where(post_id: @post)
+      @num_post_comments = @post_comments.count
   end
 
   # GET /board_profiles/new
