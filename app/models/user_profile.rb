c22b3f756a606347b1e0fd10b312e5b3a57c12f5
Nogueira-12 <tomassn2008@gmail.com>
2026-03-24 12:27:23 +0000

    edição e criação de perfis completamente funcional, para todos os users exceto admin

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,6 +2,7 @@ class UserProfile < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_one_attached :profile_picture
   has_one_attached :banner
+  accepts_nested_attributes_for :user
 
   validates_presence_of :name
 
