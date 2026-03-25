a09af2dd1e99c497fe666dfb03cd694aa27e77bd
Nogueira-12 <tomassn2008@gmail.com>
2026-03-25 16:49:07 +0000

    adicionado um captcha na criação de conta jogador. Verificar as imagens se aparecem todas corretas, com 3 do tema. os temas são: bicicletas, carros e semáforos

--- db/migrate/20260324091233_create_club_invitation_players.rb
+++ db/migrate/20260324091233_create_club_invitation_players.rb
@@ -3,7 +3,7 @@ class CreateClubInvitationPlayers < ActiveRecord::Migration[8.0]
     create_table :club_invitation_players do |t|
       t.references :club_profile, null: false, foreign_key: true
       t.references :player_profile, null: false, foreign_key: true
-      t.integer :status, default: 0, null: falsecl
+      t.integer :status, default: 0, null: false
 
       t.timestamps
     end
