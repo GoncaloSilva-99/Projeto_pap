726e060b6ec92c4ddc6eeb10a552cab2ae9f364f
GoncaloSilva-99 <goncalosilva23707@gmail.com>
2026-04-13 14:07:58 +0100

    minor fix

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,7 +1,7 @@
 class UserProfile < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_one_attached :profile_picture
-  has_one_attached :banner
+  has_one_attached :banner_picture
   accepts_nested_attributes_for :user
 
   validates_presence_of :name
